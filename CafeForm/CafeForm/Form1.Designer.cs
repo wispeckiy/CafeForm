cd28297e411020cc7e15505a67bbcd387f7475b6
wispeckiy <max.vitvitckiy@gmail.com>
2018-06-02 21:24:44 +0300

    TransparentBackground

--- CafeForm/CafeForm/Form1.Designer.cs
+++ CafeForm/CafeForm/Form1.Designer.cs
@@ -29,48 +29,40 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.ClientWish_Label = new System.Windows.Forms.Label();
+            this.Choice_Label = new System.Windows.Forms.Label();
+            this.Earned_Label = new System.Windows.Forms.Label();
+            this.Balance_Label = new System.Windows.Forms.Label();
+            this.Tries_Label = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button1.Location = new System.Drawing.Point(14, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 150);
-            this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.Black;
             this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
             this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button2.Location = new System.Drawing.Point(187, 12);
+            this.button2.Location = new System.Drawing.Point(186, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(150, 150);
             this.button2.TabIndex = 1;
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.Color.Black;
             this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
             this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button3.Location = new System.Drawing.Point(364, 12);
+            this.button3.Location = new System.Drawing.Point(12, 12);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(150, 150);
             this.button3.TabIndex = 2;
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // pictureBox1
             // 
@@ -80,54 +72,81 @@
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
-            // label1
+            // ClientWish_Label
             // 
-            this.label1.Location = new System.Drawing.Point(69, 203);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(200, 60);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Client wish";
+            this.ClientWish_Label.BackColor = System.Drawing.Color.Transparent;
+            this.ClientWish_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.ClientWish_Label.Location = new System.Drawing.Point(69, 203);
+            this.ClientWish_Label.Name = "ClientWish_Label";
+            this.ClientWish_Label.Size = new System.Drawing.Size(200, 60);
+            this.ClientWish_Label.TabIndex = 4;
+            this.ClientWish_Label.Text = "Client wish";
+            this.ClientWish_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label2
+            // Choice_Label
             // 
-            this.label2.Location = new System.Drawing.Point(321, 203);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(200, 60);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Your choice";
+            this.Choice_Label.BackColor = System.Drawing.Color.Transparent;
+            this.Choice_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Choice_Label.Location = new System.Drawing.Point(321, 203);
+            this.Choice_Label.Name = "Choice_Label";
+            this.Choice_Label.Size = new System.Drawing.Size(200, 60);
+            this.Choice_Label.TabIndex = 5;
+            this.Choice_Label.Text = "Your choice";
+            this.Choice_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label3
+            // Earned_Label
             // 
-            this.label3.Location = new System.Drawing.Point(69, 323);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(150, 40);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Earned";
+            this.Earned_Label.BackColor = System.Drawing.Color.Transparent;
+            this.Earned_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Earned_Label.Location = new System.Drawing.Point(69, 323);
+            this.Earned_Label.Name = "Earned_Label";
+            this.Earned_Label.Size = new System.Drawing.Size(150, 40);
+            this.Earned_Label.TabIndex = 6;
+            this.Earned_Label.Text = "Earned";
+            this.Earned_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label4
+            // Balance_Label
             // 
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.label4.Location = new System.Drawing.Point(67, 490);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(200, 50);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Balance:";
+            this.Balance_Label.BackColor = System.Drawing.Color.Transparent;
+            this.Balance_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Balance_Label.Location = new System.Drawing.Point(67, 490);
+            this.Balance_Label.Name = "Balance_Label";
+            this.Balance_Label.Size = new System.Drawing.Size(200, 50);
+            this.Balance_Label.TabIndex = 7;
+            this.Balance_Label.Text = "Balance:";
+            this.Balance_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label5
+            // Tries_Label
             // 
-            this.label5.Location = new System.Drawing.Point(530, 12);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(150, 60);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Try:";
+            this.Tries_Label.BackColor = System.Drawing.Color.Transparent;
+            this.Tries_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Tries_Label.Location = new System.Drawing.Point(530, 12);
+            this.Tries_Label.Name = "Tries_Label";
+            this.Tries_Label.Size = new System.Drawing.Size(150, 60);
+            this.Tries_Label.TabIndex = 8;
+            this.Tries_Label.Text = "Tries";
             // 
             // label6
             // 
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.label6.Location = new System.Drawing.Point(672, 151);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(263, 70);
             this.label6.TabIndex = 9;
             this.label6.Text = "State:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(359, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 150);
+            this.button1.TabIndex = 10;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
@@ -136,16 +155,16 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(947, 602);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Tries_Label);
+            this.Controls.Add(this.Balance_Label);
+            this.Controls.Add(this.Earned_Label);
+            this.Controls.Add(this.Choice_Label);
+            this.Controls.Add(this.ClientWish_Label);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -156,17 +175,16 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label ClientWish_Label;
+        private System.Windows.Forms.Label Choice_Label;
+        private System.Windows.Forms.Label Earned_Label;
+        private System.Windows.Forms.Label Balance_Label;
+        private System.Windows.Forms.Label Tries_Label;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
     }
 }
 
