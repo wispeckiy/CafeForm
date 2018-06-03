609b069fd406506a334fa1eb8df8b1deff0a3f76
wispeckiy <max.vitvitckiy@gmail.com>
2018-06-03 18:45:50 +0300

    nothing

---
+++ CafeForm/CafeForm/ClassesOld/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe
+{
+    abstract class Client : IClient
+    {
+        protected int tryCount;
+        protected int state;
+
+        public abstract int Pay();
+
+        public int RandomWish(int n)
+        {
+            return new Random().Next(0, n);
+        }
+
+        public abstract void StateIncrement();
+
+        public abstract void StateDecrement();
+    }
+}
---
+++ CafeForm/CafeForm/ClassesOld/View.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe
+{
+    class View
+    {
+
+        public void Game(int balance, int count, string state)
+        {
+            Console.WriteLine("Current cafe balance: {0}", balance);
+            Console.WriteLine("Try to guess your client`s wish.");
+            Console.WriteLine("Client {0}, count of tries: {1}", state, count);
+        }
+
+        public void printFood(List<string> l)
+        {
+            for(int i = 0; i < l.Count; i++)
+                Console.WriteLine("{0}. {1}", i+1, l[i]);
+        }
+
+        public int makeChoice(List<string> l)
+        {
+            int i;
+            Console.WriteLine("0. Exit");
+            printFood(l);
+            Console.Write("Your choice: ");
+            int.TryParse(Console.ReadLine(), out (i));
+            if (i > l.Count)
+                return -1;
+            return i - 1;
+        }
+
+        public void current(string choice, string wish)
+        {
+            Console.WriteLine("\nClient`s wish was {0}", wish);
+            Console.WriteLine("Your choice was {0}\n", choice);
+        }
+    }
+}
