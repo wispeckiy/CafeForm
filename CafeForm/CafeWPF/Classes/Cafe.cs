4f9d599d2064071eecc7e56c18572e906c631850
Inna Balabon <1inna.balabon@gmail.com>
2018-09-08 15:55:38 +0300

    Classes

---
+++ CafeForm/CafeWPF/Classes/Cafe.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace CafeWPF.Classes
+{
+    class Cafe
+    {
+        int balance;
+        public CurrentClient Client { get; set; }
+        Food food;
+
+        public Cafe()
+        {
+            balance = 10;
+            food = new Classes.Food();
+         //   Client = new CurrentClient();
+        }
+
+        public void ChangeBalance(int value)
+        {
+            balance += value;
+        }
+
+        public int GetBalance() { return balance; }
+
+        public Image getFoodImage(int i) {
+            return food[i];
+        }
+
+        public void NewClient()
+        {
+            Client = new CurrentClient();
+        }
+
+        //public List<Image> getFoodImages()
+        //{
+        //    List<Image> list= new List<Image>();
+        //    for (int i = 0; i < food)
+        //        list.Add(im);
+        //    return list;
+        //}
+
+        public Image GetClientWish()
+        {
+            return food[Client.RandomWish(food.GetLenght())];
+        }
+        
+        public int CheckBalance()
+        {
+            if (balance >= 0 && balance < 100) return 0;
+            else if (balance >= 100) return 1;
+            else return -1;
+        }
+
+        public int EndClientCheck()  // -1 - програш, 0 - продовження гри, 1 - виграш
+        {
+            if (Client.TryCount == 0)
+            {
+                if (Client.State == 0) return -1;
+                else return 1;
+            }
+            if (Client.State == 0) return -1;
+            if (Client.State == 3) return 1;
+            return 0;
+        }
+
+        public int EndGameCheck()
+        {
+            if (balance >= 100) return 1;
+            else if (balance < 0) return -1;
+            else return 0;
+        }
+    }
+}
---
+++ CafeForm/CafeWPF/Classes/CurrentClient.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CafeWPF.Classes
+{
+    class CurrentClient : Client
+    {
+        readonly int manType;
+        bool flagWin = false;
+        public CurrentClient()
+        {
+            tryCount = 3;
+            Random rand = new Random();
+            manType = rand.Next(0, 6);
+            state = rand.Next(1, 3);
+            myImage = images[manType, state];
+        }
+
+        public Image MyImage
+        {
+            get { return myImage; }
+            set { myImage = value; }
+        }
+
+        public int ManType
+        {
+            get { return manType; }
+        }
+
+        public int State
+        {
+            get { return state; }
+        }
+
+        public int TryCount
+        {
+            get { return tryCount; }
+        }
+
+        public override void StateIncrement()
+        {
+            state++;
+            tryCount--;
+            myImage = images[manType, state];
+        }
+
+        public override void StateDecrement()
+        {
+            state--;
+            tryCount--;
+            myImage = images[manType, state];
+        }
+
+        public override int Pay()
+        {
+            if (state == 0) return -10;
+            else return state * 10;
+        }
+
+        
+    }
+}
